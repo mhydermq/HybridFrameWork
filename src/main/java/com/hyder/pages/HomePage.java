51f84ea95112bd22d0bca765d385cf30d84eef96
M Hyder <37790098+mhydermq@users.noreply.github.com>
2024-05-30 15:34:54 -0400

    Update HomePage.java

--- src/main/java/com/hyder/pages/HomePage.java
+++ src/main/java/com/hyder/pages/HomePage.java
@@ -9,21 +9,7 @@ import com.hyder.utils.Log;
 
 public class HomePage extends BasePage {
 
-<<<<<<< HEAD
-	public HomePage(WebDriver driver) {
-		super(driver);
-	}
 
-	// Element Locators for username textbox
-	@FindBy(name = "userName")
-	private WebElement userName;
-
-	public WebElement getUserName(){
-
-		try{
-			elementDetails.put(userName, "User Name text box");
-
-=======
 	public HomePage(WebDriver driver) throws Exception {
 		super(driver);
 	}
@@ -34,17 +20,13 @@ public class HomePage extends BasePage {
 	public WebElement getUserName(){
 		try{
 			elementDetails.put(userName, "User Name text box");
->>>>>>> cda4c41 (HybridFrameWork)
 		}catch(Exception e){
 			
 		}
 
 		return userName;
 	}
-<<<<<<< HEAD
 
-=======
->>>>>>> cda4c41 (HybridFrameWork)
 	// Element Locators for password textbox
 	@FindBy(name = "password")
 	private WebElement password;
@@ -62,12 +44,8 @@ public class HomePage extends BasePage {
 	@FindBy(xpath = "//input[@name='login']")
 	private WebElement login;
 
-<<<<<<< HEAD
-	public WebElement getLogin() {
-		
-=======
+
 	public WebElement getLogin() {		
->>>>>>> cda4c41 (HybridFrameWork)
 		try{
 			elementDetails.put(login, "Login button");
 		}catch(Exception e){
@@ -75,14 +53,9 @@ public class HomePage extends BasePage {
 		}
 		return login;
 	}
-<<<<<<< HEAD
 
 	// Login method. Notice there is no return type
-	public FlightFinderPage loginUser(String uname, String pwd) {
-=======
-	// Login method. Notice there is no return type
 	public FlightFinderPage loginUser(String uname, String pwd) throws Exception {
->>>>>>> cda4c41 (HybridFrameWork)
 
 		// Enter username
 		EnterText(getUserName(), uname);
@@ -105,8 +78,5 @@ public class HomePage extends BasePage {
 		return new FlightFinderPage(driver);
 
 	}
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> cda4c41 (HybridFrameWork)
+
