a5a841f1c3cf0ae233074627bfc71454569820a1
M Hyder <37790098+mhydermq@users.noreply.github.com>
2024-05-30 15:57:50 -0400

    Update Constants.java

--- src/main/java/com/hyder/utils/Constants.java
+++ src/main/java/com/hyder/utils/Constants.java
@@ -1,23 +1,15 @@
 package com.hyder.utils;
 
 import java.text.SimpleDateFormat;
-
 import java.util.Date;
 
-
-
 public class Constants {
-
 	// TestResults folder location
 	public static final String PATH_RESULTS = System.getProperty("user.dir") + "/TestResults/";
 
 	// Test Data Excel location
-<<<<<<< HEAD
-	public static final String PATH_DATA = System.getProperty("user.dir") + "/src/main/java/com/hyder/data/Data.xlsx";
-=======
 	public static final String PATH_DATA = System.getProperty("user.dir") 
 			+ "/src/main/java/com/hyder/data/Data.xlsx";
->>>>>>> cda4c41 (HybridFrameWork)
 
 	// Test Data Excel Sheet name
 	public static final String SHEETNAME = "Sheet1";
@@ -45,20 +37,6 @@ public class Constants {
 	public static final String reportPath = filePath + "/RESULT_LOG";
 	public static final String imagePath = filePath + "/IMAGES";
 
-
-<<<<<<< HEAD
-	public static final String reportFileName = reportPath + "/" + "TESTREPORT_" + reportDate.format(date) + "TC.html";
-	public static final String screenshotFileName = reportPath + "/SCREENSHOTS" + "TESTREPORT_"
-			+ reportDate.format(date) + "TC.html";
-
-	public static final String screenshotFilePath = screenshotFileName + "/SCREENSHOTS" + "TEST"
-			+ reportDate.format(date) + "/";
-
-	public static String sScreenshotFilepath = filePath + "/Screenshots/" + "IOLS_Screenshot_" + reportDate.format(date)
-			+ "/";
-
-	public static String sReportFileName = reportPath + "/" + "IOLSTestReport_" + reportDate.format(date) + ".html";
-=======
 	public static final String reportFileName = reportPath + "/" + "TESTREPORT_" 
 	+ reportDate.format(date) + "TC.html";
 	
@@ -72,7 +50,7 @@ public class Constants {
 			+ "/";
 	public static String sReportFileName = reportPath + "/" + 
 			"IOLSTestReport_" + reportDate.format(date) + ".html";
->>>>>>> cda4c41 (HybridFrameWork)
+
 
 
 
