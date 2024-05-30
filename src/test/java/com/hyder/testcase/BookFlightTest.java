5c64ab907da8a0b007421252beeed08382de277a
M Hyder <37790098+mhydermq@users.noreply.github.com>
2024-05-30 15:46:07 -0400

    Update BookFlightTest.java

--- src/test/java/com/hyder/testcase/BookFlightTest.java
+++ src/test/java/com/hyder/testcase/BookFlightTest.java
@@ -1,8 +1,5 @@
 package com.hyder.testcase;
 
-<<<<<<< HEAD
-import org.testng.annotations.Test;
-=======
 import java.lang.reflect.Method;
 
 import org.apache.log4j.xml.DOMConfigurator;
@@ -12,23 +9,12 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
->>>>>>> cda4c41 (HybridFrameWork)
 import com.hyder.pages.HomePage;
 import com.hyder.utils.Config;
 import com.hyder.utils.Constants;
 import com.hyder.utils.ExcelUtil;
 import com.hyder.utils.Log;
 import com.hyder.utils.Reports;
-<<<<<<< HEAD
-import org.testng.annotations.BeforeMethod;
-import java.lang.reflect.Method;
-import org.apache.log4j.xml.DOMConfigurator;
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-=======
->>>>>>> cda4c41 (HybridFrameWork)
-
 
 public class BookFlightTest {
 	WebDriver driver;
@@ -37,14 +23,8 @@ public class BookFlightTest {
 
   // First Test Case	
   @Test
-<<<<<<< HEAD
-  public void firstTestCase() {
-	  homePage = new HomePage(driver);
-
-=======
   public void firstTestCase() throws Exception {
 	  homePage = new HomePage(driver);
->>>>>>> cda4c41 (HybridFrameWork)
 	  try {
 		homePage.navigateToURL(ExcelUtil.getCellData(1, 1));
 		homePage.loginUser(ExcelUtil.getCellData(1, 2), 
@@ -53,16 +33,10 @@ public class BookFlightTest {
 		e.printStackTrace();
 	}
   }
-<<<<<<< HEAD
-  
-  //Second Test Case	 
-  @Test
-  public void secondTestCase() {
-=======
+
   //Second Test Case	 
   @Test
   public void secondTestCase() throws Exception {
->>>>>>> cda4c41 (HybridFrameWork)
 	  homePage = new HomePage(driver);
 	  try{
 		  homePage.navigateToURL(ExcelUtil.getCellData(2, 1));
@@ -71,21 +45,12 @@ public class BookFlightTest {
 
 	  } catch (Exception e) {
 			e.printStackTrace();
-<<<<<<< HEAD
-	  }
-  }
-	 
-  // Before Method to start logging
-  @BeforeMethod
-  public void beforeMethod(Method method) {
-
-=======
 	}
   } 
+	
   // Before Method to start logging
   @BeforeMethod
   public void beforeMethod(Method method) throws Exception {
->>>>>>> cda4c41 (HybridFrameWork)
 	  Log.startTestCase(method.getName());
 	  report.startTest(method.getName());
 	  System.out.println(Config.getProp().getProperty("selenium.browser")); 
@@ -94,19 +59,6 @@ public class BookFlightTest {
   // After Method to end logging
   @AfterMethod
   public void afterMethod() {
-<<<<<<< HEAD
-
-	    homePage.endTest();
-		homePage.endReport();
-		Log.endTestCase("firstTestCase");
-
-  }
-
-    //Before Class to initialize Log4j
-	@BeforeClass
-
-	public void beforeClass() {
-=======
 	    homePage.endTest();
 		homePage.endReport();
 		Log.endTestCase("firstTestCase");
@@ -114,7 +66,6 @@ public class BookFlightTest {
     //Before Class to initialize Log4j
 	@BeforeClass
 	public void beforeClass() throws Exception {
->>>>>>> cda4c41 (HybridFrameWork)
 		 System.setProperty(Config.getProp().getProperty("logfoldername"), Constants.logFolderName);
 		 System.setProperty(Config.getProp().getProperty("logfilename"), Constants.logFileName);
 
@@ -128,8 +79,5 @@ public class BookFlightTest {
 		}
 		 report = new Reports();
 	}
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> cda4c41 (HybridFrameWork)
+
